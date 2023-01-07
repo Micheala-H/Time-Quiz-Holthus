ed43a53134520ddb73a14e96f9e2f0b171dead78
Micheala-H <mholthus17@gmail.com>
2023-01-06 23:44:14 -0600

    added starting code and some basic start to js

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+var questions = [
+    {
+        question: "what java"
+    } 
+    
+    {
+        question: "what JS", answer: "gafiue", possible: ["hgoi", "gafiue", "", ""]
+    }
+]
+
+var questionCounter = 0
+
+function answerQstn{
+
+}
